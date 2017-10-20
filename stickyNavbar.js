629377e671815872bcdc7987e0d8d08ca5e23bb1
Nathaniel Clark <Nclark777@gmail.com>
2017-10-20 05:33:15 -0400

    added nc_ to navbar

--- stickyNavbar.js
+++ stickyNavbar.js
@@ -1,12 +1,12 @@
 // JavaScript Document
 
-var navbar = document.getElementById("navbar");
+var nc_navbar = document.getElementById("navbar");
 var sticky = nc_navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     nc_navbar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    nc_navbar.classList.remove("sticky");
   }
 }
